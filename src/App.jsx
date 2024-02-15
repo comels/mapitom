471e5d1217822f14d5c591f34a91ec07ead3d71c
comels <come.l.s@hotmail.fr>
2024-02-15 11:17:23 +0100

    scroll top

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import Contact from './pages/Contact';
-import Test from './pages/Test';
-import Gallery from './pages/Gallery';
-import NotFound from './pages/NotFound';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from "react";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import Test from "./pages/Test";
+import Gallery from "./pages/Gallery";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 
 const App = () => {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  };
+
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/test" element={<Test />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/galerie" element={<Gallery />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
