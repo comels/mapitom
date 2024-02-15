2b1419c84ada3ebd7e3c264b260d0d9c2e02bfa5
comels <come.l.s@hotmail.fr>
2024-02-15 12:28:27 +0100

    mt nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Illustration from "../components/IllustrationHeader";
 
 const Menu = () => {
   return (
-    <div className="mt-20">
+    <div className="mt-14">
       <div
         className="bg-cover h-screen bg-center flex flex-col justify-center"
         style={{ backgroundImage: "url('bandeau-mapitom.jpeg')" }}
